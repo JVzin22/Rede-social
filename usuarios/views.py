b273ecefd3a4ebf8661b75ce71010bee0489d57c
Joao Victor <joaovictor.programacao@simplesvarejo.com.br>
2025-10-30 18:41:30 -0300

    Inicio da criacao da tela de cadastro de usuário

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 
 def cadastro_usuario(request):
-    return render(request, 'cadastro_usuario.html')
+    if request.method == 'POST':
+        username = request.POST.get('usuario')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+
+        existe_usuario_nome = User.objects.filter(username=username).exists()
+        existe_usuario_email = User.objects.filter(email=email).exists()
+
+        if password != password_confirm:
+            messages.error(request, "As senhas não coincidem.")
+            return redirect('cadastro_usuario')
+        try:
+            validate_password(password)
+        except Exception as e:
+            for error in e:
+                messages.error(request, error)
+            return redirect('cadastro_usuario')
+
+    else:
+        return render(request, 'cadastro_usuario.html')
 
 def login(request):
     return render(request, 'login.html')
